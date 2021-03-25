8965a0e9c073b36ae5d1c8777622682297317d18
DoeyJonuts <joeyefarah@gmail.com>
2021-03-24 23:06:40 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+//https://icanhazdadjoke.com/
+
+const getDadJoke = async () => {
+    try{
+        const config = { 
+            headers: 
+            { Accept: 'application/json' } 
+        }
+        const res = await axios.get('https://icanhazdadjoke.com/', config)
+        const dadJoke = (res.data.joke);
+        return dadJoke;
+    } catch(e){
+        console.log('Sorry, no jokes :-(', e)
+    }
+
+}
+
+const addNewJoke = async () => {
+    const jokeContent = await getDadJoke();
+    const jokeList = document.querySelector('#jokeList');
+    const newLi = document.createElement('li');
+    newLi.append(jokeContent);
+    jokeList.appendChild(newLi);
+}
+
+
+
+const dadJokeButton = document.querySelector('#dadJokeButton');
+
+dadJokeButton.addEventListener('click', addNewJoke);
+
+
+
